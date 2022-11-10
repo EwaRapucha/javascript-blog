113df73d5450f7172e15b24f454982af8df4b20e
Ewa Rapucha <annarapucha@gmail.com>
2022-11-10 16:58:28 +0000

    Add active class

--- js/script.js
+++ js/script.js
@@ -7,9 +7,11 @@
 
 const titleClickHandler = function (event) {
   console.log(event);
+  event.preventDefault();
+  const clickedElement = this;
   console.log("Link was clicked!");
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll(".titles a.active");
 
@@ -17,9 +19,12 @@ const titleClickHandler = function (event) {
     activeLink.classList.remove("active");
   }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+  console.log("clickedElement:", clickedElement);
+  clickedElement.classList.add("active");
+
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll(".posts article.active");
 
   for (let activeArticle of activeArticles) {
