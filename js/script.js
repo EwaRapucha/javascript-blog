3d1dcd914fc36bcf130e2884f0a8ba4d721cd51e
Ewa Rapucha <annarapucha@gmail.com>
2022-11-10 14:29:30 +0000

    Change js/script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 "use strict";
+
 /*document.getElementById("test-button").addEventListener("click", function () {
   const links = document.querySelectorAll(".titles a");
   console.log("links:", links);
@@ -10,9 +11,20 @@ const titleClickHandler = function (event) {
 
   /* remove class 'active' from all article links  */
 
+  const activeLinks = document.querySelectorAll(".titles a.active");
+
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove("active");
+  }
+
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll(".posts article.active");
+
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove("active");
+  }
 
   /* get 'href' attribute from the clicked link */
 
